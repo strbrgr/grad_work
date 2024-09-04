98dcf26335368629bd067c466d4cf0293a0ad949
Jochen Stierberger <jocsti@web.de>
2024-09-04 06:30:53 -0500

    fix: Test other operators

--- oak/csc240/1/fracDriver.cpp
+++ oak/csc240/1/fracDriver.cpp
@@ -11,6 +11,7 @@ int main() {
   FractionType three = one + two;
   three.print();
 
+  // Test the + operator
   FractionType x;
   FractionType y;
   x.Initialize(1, 3);
@@ -25,4 +26,28 @@ int main() {
   g.Initialize(15, 20);
   g.reduce();
   g.print();
+
+  // Test the - operator
+  FractionType four;
+  FractionType five;
+  four.Initialize(1, 3);
+  five.Initialize(4, 3);
+  FractionType six = five - four;
+  six.print();
+
+  // Test the / operator
+  FractionType seven;
+  FractionType eight;
+  seven.Initialize(5, 3);
+  eight.Initialize(8, 9);
+  FractionType nine = seven / eight;
+  nine.print();
+
+  // Test the * operator
+  FractionType ten;
+  FractionType eleven;
+  ten.Initialize(3, 2);
+  eleven.Initialize(5, 12);
+  FractionType twelve = ten * eleven;
+  twelve.print();
 }
