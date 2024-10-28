fa8b66298a33b4eb8c3a77878944a34c769e88e7
Jochen Stierberger <jocsti@web.de>
2024-10-28 06:42:19 -0500

    fix: Implementation

--- oakton/csc255/1/U1A1/binary_search.cpp
+++ oakton/csc255/1/U1A1/binary_search.cpp
@@ -3,29 +3,20 @@
 const int NOT_FOUND = -1;
 
 template <typename Comparable>
-// FUNCTION: Recursive Function to implement binary search on a vector.
-// POST: Vector, Comparable, low, and high are passed as input
-//
 int binarySearch(const std::vector<Comparable> &a, const Comparable &x, int low,
                  int high) {
-  // Base case, low pointer went beyond high pointer
-  if (low > high) {
-    return NOT_FOUND;
+  while (low <= high) {
+    int mid = (low + high) / 2;
+    Comparable midPointValue = a[mid];
+
+    if (midPointValue <= x) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
   }
 
-  int mid = (low + high) / 2;
-  Comparable midPointValue = a[mid];
-  // general case,
-  if (midPointValue == x) {
-    return mid;
-  }
-
-  // Recursive case
-  if (midPointValue < x) {
-    return binarySearch(a, x, mid + 1, high);
-  }
-
-  return binarySearch(a, x, low, mid - 1);
+  return low - 1;
 }
 
 int main() {
@@ -45,9 +36,11 @@ int main() {
 
 // 1) What is the count of comparisons of binary_search given an input array of
 // size N for the original version?
-// Worst case of O(log N) due to splitting the input in half.
+// The original version has a time complexity of O(log N) with N being the size
+// of the input array.
 //
 // 2) What is the count of comparisons of
 // binary_search given an input array of size N for the updated and improved
 // version?
-// Worst case of O(log N) due to splitting the input in half.
+// The improved versioon of binary search also has a time complexity of O(log
+// N), but due to the reduced number of comparisons it might be slighlty better.
